6d6a2187cf0125cc786980a64f47f3019339344a
jpbool0 <151468310+jpbool0@users.noreply.github.com>
2024-09-07 13:56:31 -0300

    Yay deu certo
    Yay, deu certo

--- Programas em C#/4 bimestre/winforms/AplicacaoBancoDados/CadastroBanco/CadastroBanco/Form1.cs
+++ Programas em C#/4 bimestre/winforms/AplicacaoBancoDados/CadastroBanco/CadastroBanco/Form1.cs
@@ -5,7 +5,7 @@ using static CadastroBanco.Form1;
 
 namespace CadastroBanco
 {
-    public partial class Form1 : Form
+    public partial class Form1 : RoundRectForm
     {
         // Lista para armazenar os dados localmente
         private List<Produto> produtos = new List<Produto>();
@@ -185,6 +185,7 @@ namespace CadastroBanco
 
         private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
+        }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
