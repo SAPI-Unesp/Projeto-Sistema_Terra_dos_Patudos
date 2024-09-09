878ef5a6de8491ce3b58c3a48d4b91694dbd6b7a
Thiago H. M. Fonseca <th.fonseca@unesp.br>
2024-09-09 13:48:37 -0300

    ffoooi
    fooi

--- CadastroProduto/CadastroBanco/Form1.Designer.cs
+++ CadastroProduto/CadastroBanco/Form1.Designer.cs
@@ -138,6 +138,7 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(45, 52);
             this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(75, 20);
             this.textBox1.TabIndex = 5;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
