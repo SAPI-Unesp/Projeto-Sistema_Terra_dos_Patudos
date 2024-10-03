354ecceb17051617ba3b5f58d8c2c70d4dd9bcba
jpbool0 <151468310+jpbool0@users.noreply.github.com>
2024-10-03 14:17:39 -0300

    A
    A

--- CadastroProduto/CadastroBanco/Form2.Designer.cs
+++ CadastroProduto/CadastroBanco/Form2.Designer.cs
@@ -36,6 +36,7 @@
             this.btnAtualizar = new System.Windows.Forms.Button();
             this.btnDeletar = new System.Windows.Forms.Button();
             this.dataGridViewDados = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.categoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.descricao = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -44,14 +45,14 @@
             this.dataAdicao = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.comprador = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.telefone = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.vendido = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDados)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(33, 31);
+            this.label1.Location = new System.Drawing.Point(12, 21);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(79, 13);
             this.label1.TabIndex = 0;
@@ -60,60 +61,70 @@
             // 
             // btnAdicionar
             // 
-            this.btnAdicionar.Location = new System.Drawing.Point(21, 353);
+            this.btnAdicionar.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnAdicionar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdicionar.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAdicionar.Location = new System.Drawing.Point(851, 26);
             this.btnAdicionar.Name = "btnAdicionar";
-            this.btnAdicionar.Size = new System.Drawing.Size(75, 23);
+            this.btnAdicionar.Size = new System.Drawing.Size(109, 40);
             this.btnAdicionar.TabIndex = 2;
-            this.btnAdicionar.Text = "btnAdicionar";
-            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = false;
             this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click_1);
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(170, 241);
+            this.txtNome.Location = new System.Drawing.Point(65, 250);
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(100, 20);
             this.txtNome.TabIndex = 3;
             // 
             // txtIdade
             // 
-            this.txtIdade.Location = new System.Drawing.Point(170, 267);
+            this.txtIdade.Location = new System.Drawing.Point(65, 276);
             this.txtIdade.Name = "txtIdade";
             this.txtIdade.Size = new System.Drawing.Size(100, 20);
             this.txtIdade.TabIndex = 4;
             // 
             // btnExibir
             // 
-            this.btnExibir.Location = new System.Drawing.Point(21, 401);
+            this.btnExibir.Location = new System.Drawing.Point(585, 25);
             this.btnExibir.Name = "btnExibir";
-            this.btnExibir.Size = new System.Drawing.Size(75, 23);
+            this.btnExibir.Size = new System.Drawing.Size(75, 41);
             this.btnExibir.TabIndex = 5;
-            this.btnExibir.Text = "btnExibir";
+            this.btnExibir.Text = "Exibir";
             this.btnExibir.UseVisualStyleBackColor = true;
             this.btnExibir.Click += new System.EventHandler(this.btnExibir_Click_1);
             // 
             // btnAtualizar
             // 
-            this.btnAtualizar.Location = new System.Drawing.Point(21, 443);
+            this.btnAtualizar.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnAtualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAtualizar.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAtualizar.Location = new System.Drawing.Point(761, 26);
             this.btnAtualizar.Name = "btnAtualizar";
-            this.btnAtualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnAtualizar.Size = new System.Drawing.Size(84, 40);
             this.btnAtualizar.TabIndex = 6;
-            this.btnAtualizar.Text = "btnAtualizar";
-            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Text = "Editar";
+            this.btnAtualizar.UseVisualStyleBackColor = false;
             this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click_1);
             // 
             // btnDeletar
             // 
-            this.btnDeletar.Location = new System.Drawing.Point(21, 482);
+            this.btnDeletar.BackColor = System.Drawing.Color.Salmon;
+            this.btnDeletar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeletar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnDeletar.Location = new System.Drawing.Point(504, 25);
             this.btnDeletar.Name = "btnDeletar";
-            this.btnDeletar.Size = new System.Drawing.Size(75, 23);
+            this.btnDeletar.Size = new System.Drawing.Size(75, 41);
             this.btnDeletar.TabIndex = 7;
-            this.btnDeletar.Text = "btnDeletar";
-            this.btnDeletar.UseVisualStyleBackColor = true;
+            this.btnDeletar.Text = "Deletar";
+            this.btnDeletar.UseVisualStyleBackColor = false;
             this.btnDeletar.Click += new System.EventHandler(this.btnDeletar_Click_1);
             // 
             // dataGridViewDados
             // 
+            this.dataGridViewDados.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dataGridViewDados.ColumnHeadersHeight = 40;
             this.dataGridViewDados.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.id,
@@ -123,11 +134,20 @@
             this.preco,
             this.dataAdicao,
             this.comprador,
-            this.telefone});
-            this.dataGridViewDados.Location = new System.Drawing.Point(127, 63);
+            this.telefone,
+            this.vendido});
+            this.dataGridViewDados.Location = new System.Drawing.Point(12, 72);
             this.dataGridViewDados.Name = "dataGridViewDados";
-            this.dataGridViewDados.Size = new System.Drawing.Size(835, 442);
+            this.dataGridViewDados.Size = new System.Drawing.Size(948, 442);
             this.dataGridViewDados.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(464, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // id
             // 
@@ -146,7 +166,7 @@
             // 
             this.descricao.HeaderText = "Descrição";
             this.descricao.Name = "descricao";
-            this.descricao.Width = 220;
+            this.descricao.Width = 260;
             // 
             // quantidade
             // 
@@ -175,19 +195,17 @@
             this.telefone.HeaderText = "Telefone do Comprador";
             this.telefone.Name = "telefone";
             // 
-            // textBox1
+            // vendido
             // 
-            this.textBox1.Location = new System.Drawing.Point(127, 28);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(464, 20);
-            this.textBox1.TabIndex = 10;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.vendido.HeaderText = "Vendido?";
+            this.vendido.Name = "vendido";
+            this.vendido.Width = 70;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1019, 563);
+            this.ClientSize = new System.Drawing.Size(970, 523);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridViewDados);
             this.Controls.Add(this.btnDeletar);
@@ -216,6 +234,7 @@
         private System.Windows.Forms.Button btnAtualizar;
         private System.Windows.Forms.Button btnDeletar;
         private System.Windows.Forms.DataGridView dataGridViewDados;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.DataGridViewTextBoxColumn id;
         private System.Windows.Forms.DataGridViewTextBoxColumn categoria;
         private System.Windows.Forms.DataGridViewTextBoxColumn descricao;
@@ -224,6 +243,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dataAdicao;
         private System.Windows.Forms.DataGridViewTextBoxColumn comprador;
         private System.Windows.Forms.DataGridViewTextBoxColumn telefone;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vendido;
     }
 }
