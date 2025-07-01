e37e1929fe55a67254b17ff1ecb9d3c642f48b56
LeonardoQ357 <116414697+LeonardoQ357@users.noreply.github.com>
2025-07-01 07:18:32 -0300

    Carrinho zero bala

--- CadastroProduto/CadastroBanco/FormVisualizarCarrinho.Designer.cs
+++ CadastroProduto/CadastroBanco/FormVisualizarCarrinho.Designer.cs
@@ -74,13 +74,12 @@
             this.quantidade,
             this.preco,
             this.qtdVendida});
-            this.dataGridViewDados.Location = new System.Drawing.Point(11, 15);
-            this.dataGridViewDados.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridViewDados.Location = new System.Drawing.Point(8, 12);
             this.dataGridViewDados.MultiSelect = false;
             this.dataGridViewDados.Name = "dataGridViewDados";
             this.dataGridViewDados.RowHeadersWidth = 51;
             this.dataGridViewDados.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridViewDados.Size = new System.Drawing.Size(839, 344);
+            this.dataGridViewDados.Size = new System.Drawing.Size(1002, 368);
             this.dataGridViewDados.TabIndex = 10;
             this.dataGridViewDados.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewDados_CellContentClick);
             this.dataGridViewDados.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewDados_CellValueChanged);
@@ -144,10 +143,9 @@
             this.btnRemover.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.btnRemover.BackColor = System.Drawing.Color.Salmon;
             this.btnRemover.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
-            this.btnRemover.Location = new System.Drawing.Point(10, 369);
-            this.btnRemover.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRemover.Location = new System.Drawing.Point(8, 388);
             this.btnRemover.Name = "btnRemover";
-            this.btnRemover.Size = new System.Drawing.Size(132, 49);
+            this.btnRemover.Size = new System.Drawing.Size(99, 40);
             this.btnRemover.TabIndex = 11;
             this.btnRemover.Text = "Remover";
             this.btnRemover.UseVisualStyleBackColor = false;
@@ -158,10 +156,9 @@
             this.btnFinalizarCompra.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnFinalizarCompra.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.btnFinalizarCompra.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnFinalizarCompra.Location = new System.Drawing.Point(863, 235);
-            this.btnFinalizarCompra.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnFinalizarCompra.Location = new System.Drawing.Point(1020, 279);
             this.btnFinalizarCompra.Name = "btnFinalizarCompra";
-            this.btnFinalizarCompra.Size = new System.Drawing.Size(265, 124);
+            this.btnFinalizarCompra.Size = new System.Drawing.Size(199, 101);
             this.btnFinalizarCompra.TabIndex = 12;
             this.btnFinalizarCompra.Text = "Finalizar\r\ncompra";
             this.btnFinalizarCompra.UseVisualStyleBackColor = false;
@@ -171,10 +168,9 @@
             // 
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(858, 66);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(1017, 54);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 14;
             this.label2.Text = "Nome:";
             // 
@@ -182,39 +178,35 @@
             // 
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(858, 117);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(1017, 95);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 16);
+            this.label3.Size = new System.Drawing.Size(52, 13);
             this.label3.TabIndex = 15;
             this.label3.Text = "Telefone:";
             // 
             // nome
             // 
             this.nome.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.nome.Location = new System.Drawing.Point(862, 86);
-            this.nome.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nome.Location = new System.Drawing.Point(1019, 70);
             this.nome.Name = "nome";
-            this.nome.Size = new System.Drawing.Size(196, 22);
+            this.nome.Size = new System.Drawing.Size(148, 20);
             this.nome.TabIndex = 16;
             // 
             // telefone
             // 
             this.telefone.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.telefone.Location = new System.Drawing.Point(862, 137);
-            this.telefone.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.telefone.Location = new System.Drawing.Point(1019, 111);
             this.telefone.Name = "telefone";
-            this.telefone.Size = new System.Drawing.Size(132, 22);
+            this.telefone.Size = new System.Drawing.Size(100, 20);
             this.telefone.TabIndex = 17;
             // 
             // label4
             // 
             this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(859, 15);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(1017, 12);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(139, 16);
+            this.label4.Size = new System.Drawing.Size(112, 13);
             this.label4.TabIndex = 20;
             this.label4.Text = "Status do Pagamento:";
             // 
@@ -226,10 +218,9 @@
             this.cbPagamento.Items.AddRange(new object[] {
             "Realizado",
             "Pendente"});
-            this.cbPagamento.Location = new System.Drawing.Point(862, 34);
-            this.cbPagamento.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbPagamento.Location = new System.Drawing.Point(1019, 28);
             this.cbPagamento.Name = "cbPagamento";
-            this.cbPagamento.Size = new System.Drawing.Size(160, 24);
+            this.cbPagamento.Size = new System.Drawing.Size(121, 21);
             this.cbPagamento.TabIndex = 22;
             this.cbPagamento.SelectedIndexChanged += new System.EventHandler(this.CbPagamento_SelectedIndexChanged);
             // 
@@ -238,10 +229,9 @@
             this.tbTotalPagar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.tbTotalPagar.Enabled = false;
             this.tbTotalPagar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbTotalPagar.Location = new System.Drawing.Point(995, 200);
-            this.tbTotalPagar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbTotalPagar.Location = new System.Drawing.Point(1119, 250);
             this.tbTotalPagar.Name = "tbTotalPagar";
-            this.tbTotalPagar.Size = new System.Drawing.Size(132, 26);
+            this.tbTotalPagar.Size = new System.Drawing.Size(100, 22);
             this.tbTotalPagar.TabIndex = 23;
             this.tbTotalPagar.TextChanged += new System.EventHandler(this.tbTotalPagar_TextChanged);
             // 
@@ -250,10 +240,9 @@
             this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
-            this.label5.Location = new System.Drawing.Point(859, 204);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(1017, 254);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(126, 20);
+            this.label5.Size = new System.Drawing.Size(105, 16);
             this.label5.TabIndex = 24;
             this.label5.Text = "Total a pagar:";
             // 
@@ -262,10 +251,9 @@
             this.btnLimparCarrinho.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.btnLimparCarrinho.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.btnLimparCarrinho.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
-            this.btnLimparCarrinho.Location = new System.Drawing.Point(150, 369);
-            this.btnLimparCarrinho.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLimparCarrinho.Location = new System.Drawing.Point(112, 388);
             this.btnLimparCarrinho.Name = "btnLimparCarrinho";
-            this.btnLimparCarrinho.Size = new System.Drawing.Size(132, 49);
+            this.btnLimparCarrinho.Size = new System.Drawing.Size(99, 40);
             this.btnLimparCarrinho.TabIndex = 25;
             this.btnLimparCarrinho.Text = "Limpar";
             this.btnLimparCarrinho.UseVisualStyleBackColor = false;
@@ -280,22 +268,19 @@
             "Dinheiro",
             "Cartão Debito",
             "Cartão Crédito",
-            "Transferência Bancária",
-            "Crédito Loja"});
-            this.comboBox1.Location = new System.Drawing.Point(863, 193);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            "Transferência Bancária"});
+            this.comboBox1.Location = new System.Drawing.Point(1020, 157);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(160, 24);
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 26;
             // 
             // label1
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(859, 174);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(1017, 141);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(148, 16);
+            this.label1.Size = new System.Drawing.Size(116, 13);
             this.label1.TabIndex = 27;
             this.label1.Text = "Formas de Pagamento:";
             // 
@@ -303,10 +288,9 @@
             // 
             this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(860, 228);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(1018, 185);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(244, 16);
+            this.label6.Size = new System.Drawing.Size(193, 13);
             this.label6.TabIndex = 29;
             this.label6.Text = "Data prevista de pagamento (opcional):";
             // 
@@ -314,10 +298,9 @@
             // 
             this.dataPrazo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.dataPrazo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dataPrazo.Location = new System.Drawing.Point(862, 248);
-            this.dataPrazo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataPrazo.Location = new System.Drawing.Point(1019, 202);
             this.dataPrazo.Name = "dataPrazo";
-            this.dataPrazo.Size = new System.Drawing.Size(265, 30);
+            this.dataPrazo.Size = new System.Drawing.Size(200, 26);
             this.dataPrazo.TabIndex = 30;
             this.dataPrazo.Value = new System.DateTime(2024, 11, 26, 0, 0, 0, 0);
             // 
@@ -325,10 +308,9 @@
             // 
             this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox2.Image = global::CadastroBanco.Properties.Resources.duvida;
-            this.pictureBox2.Location = new System.Drawing.Point(1099, 390);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Location = new System.Drawing.Point(1197, 405);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(45, 31);
+            this.pictureBox2.Size = new System.Drawing.Size(34, 25);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 31;
             this.pictureBox2.TabStop = false;
@@ -336,9 +318,9 @@
             // 
             // FormVisualizarCarrinho
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1155, 433);
+            this.ClientSize = new System.Drawing.Size(1239, 440);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.dataPrazo);
             this.Controls.Add(this.label6);
@@ -357,7 +339,6 @@
             this.Controls.Add(this.btnRemover);
             this.Controls.Add(this.dataGridViewDados);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormVisualizarCarrinho";
             this.Text = "Carrinho";
             this.Load += new System.EventHandler(this.FormVisualizarCarrinho_Load);
